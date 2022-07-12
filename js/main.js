f0316ed30b107bcfad2bc74e30167a0395d61e19
Towa Quimbayo <towacurtis@hotmail.com>
2022-07-12 14:52:45 -0700

    Completed Layout for Work Page Desktop View
    - Template for navbar and floating socials
    - Completed work page layout

--- js/main.js
+++ js/main.js
@@ -1,20 +1,39 @@
+/**
+ * Constant Variables.
+ */
+const mainFloatSocials = $('#floatingSocials');
+
 $(document).ready(function () {
-    var mainFloatSocials = $('#floatingSocials');
+    loadNav();
+    
     mainFloatSocials.addClass('enter');
     mainFloatSocials.click(openElement);
-
-    function openElement() {
-        $('.linkedinIcon').toggleClass('enter');
-        setTimeout(() => {
-            $('.githubIcon').toggleClass('enter');
-        }, 100);
-        setTimeout(() => {
-            $('.mailIcon').toggleClass('enter');
-        }, 200);
-    }
-
     mainFloatSocials.click(function () {
         $('#socialsIcon').toggleClass('bi-chat-fill');
         $('#socialsIcon').toggleClass('bi-x');
     });
 });
+
+/**
+ * Load the Navbar.
+ */
+function loadNav() {
+    $('#navTemp').load('../temp/nav.html', function () {
+        $('.navLink').each(function () {
+            $(this).toggleClass('active', this.getAttribute('href') === location.pathname);
+        });
+    });
+}
+
+/**
+ * Toggle floating socials contact action button.
+ */
+function openElement() {
+    $('.linkedinIcon').toggleClass('enter');
+    setTimeout(() => {
+        $('.githubIcon').toggleClass('enter');
+    }, 100);
+    setTimeout(() => {
+        $('.mailIcon').toggleClass('enter');
+    }, 200);
+}
