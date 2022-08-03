ca0072667e50c0168e63c23c3081e598303b76a3
Towa Quimbayo <towacurtis@hotmail.com>
2022-08-03 16:14:44 -0700

    Completed Mobile Optimization for all work pages

--- js/singleWork.js
+++ js/singleWork.js
@@ -11,62 +11,87 @@ setTimeout(() => {
 
 $(document).ready(function () {
     window.onscroll = function () {
-        console.log(window.pageYOffset)
-        if (window.pageYOffset >= 100 && window.pageYOffset < 1400) {
-            for (var i = 0; i < navLinksColor.length; i++) {
-                navLinksColor[i].classList.add('changeBg');
+        if (window.screen.width >= 1800) {
+            if (window.pageYOffset >= 100 && window.pageYOffset < 1400) {
+                changeNavColor();
+            } else if (window.pageYOffset >= 2000 && window.pageYOffset < 2600) {
+                activateProjImg1();
+            } else if (window.pageYOffset > 2900 && window.pageYOffset < 3800) {
+                activateProjImg2();
+            } else if (window.pageYOffset > 4000 && window.pageYOffset < 4600) {
+                activateProjImg3();
+            } else if (window.pageYOffset > 5200 && window.pageYOffset < 6300) {
+                activateProjImg4and5();
+            } else {
+                deactivateAllProjImgs();
             }
-            document.querySelector('#projGallerySec1').classList.remove('active');
-            document.querySelector('#projGallerySec2').classList.remove('active');
-            document.querySelector('#projGallerySec3').classList.remove('active');
-            document.querySelector('#projGallerySec4').classList.remove('active');
-            document.querySelector('#projGallerySec5').classList.remove('active');
-        } else if (window.pageYOffset >= 2000 && window.pageYOffset < 2600) {
-            for (var i = 0; i < navLinksColor.length; i++) {
-                navLinksColor[i].classList.remove('changeBg');
+        } else if (window.screen.width < 1800 && window.screen.width >= 1300) {
+            if (window.pageYOffset >= 100 && window.pageYOffset < 1500) {
+                changeNavColor();
+            } else if (window.pageYOffset >= 2000 && window.pageYOffset < 2600) {
+                activateProjImg1();
+            } else if (window.pageYOffset > 2800 && window.pageYOffset < 3200) {
+                activateProjImg2();
+            } else if (window.pageYOffset > 3600 && window.pageYOffset < 4000) {
+                activateProjImg3();
+            } else if (window.pageYOffset > 4200 && window.pageYOffset < 5000) {
+                activateProjImg4and5();
+            } else {
+                deactivateAllProjImgs();
             }
-            document.querySelector('#projGallerySec1').classList.add('active');
-            document.querySelector('#projGallerySec2').classList.remove('active');
-            document.querySelector('#projGallerySec3').classList.remove('active');
-            document.querySelector('#projGallerySec4').classList.remove('active');
-            document.querySelector('#projGallerySec5').classList.remove('active');
-        } else if (window.pageYOffset > 2900 && window.pageYOffset < 3800) {
-            for (var i = 0; i < navLinksColor.length; i++) {
-                navLinksColor[i].classList.remove('changeBg');
+        } else if (window.screen.width < 1300 && window.screen.width >= 992) {
+            if (window.pageYOffset >= 100 && window.pageYOffset < 1600) {
+                changeNavColor();
+            } else if (window.pageYOffset >= 2000 && window.pageYOffset < 2600) {
+                activateProjImg1();
+            } else if (window.pageYOffset > 2800 && window.pageYOffset < 3200) {
+                activateProjImg2();
+            } else if (window.pageYOffset > 3600 && window.pageYOffset < 4000) {
+                activateProjImg3();
+            } else if (window.pageYOffset > 4200 && window.pageYOffset < 5000) {
+                activateProjImg4and5();
+            } else {
+                deactivateAllProjImgs();
             }
-            document.querySelector('#projGallerySec1').classList.remove('active');
-            document.querySelector('#projGallerySec2').classList.add('active');
-            document.querySelector('#projGallerySec3').classList.remove('active');
-            document.querySelector('#projGallerySec4').classList.remove('active');
-            document.querySelector('#projGallerySec5').classList.remove('active');
-        } else if (window.pageYOffset > 4000 && window.pageYOffset < 4600) {
-            for (var i = 0; i < navLinksColor.length; i++) {
-                navLinksColor[i].classList.remove('changeBg');
+        } else if (window.screen.width < 992 && window.screen.width >= 768) {
+            if (window.pageYOffset >= 2400 && window.pageYOffset < 2800) {
+                activateProjImg1();
+            } else if (window.pageYOffset > 3000 && window.pageYOffset < 3400) {
+                activateProjImg2();
+            } else if (window.pageYOffset > 3500 && window.pageYOffset < 4000) {
+                activateProjImg3();
+            } else if (window.pageYOffset > 4100 && window.pageYOffset < 4800) {
+                activateProjImg4and5();
+            } else {
+                deactivateAllProjImgs();
             }
-            document.querySelector('#projGallerySec1').classList.remove('active');
-            document.querySelector('#projGallerySec2').classList.remove('active');
-            document.querySelector('#projGallerySec3').classList.add('active');
-            document.querySelector('#projGallerySec4').classList.remove('active');
-            document.querySelector('#projGallerySec5').classList.remove('active');
-        } else if (window.pageYOffset > 5200 && window.pageYOffset < 6300) {
-            for (var i = 0; i < navLinksColor.length; i++) {
-                navLinksColor[i].classList.remove('changeBg');
+        } else if (window.screen.width < 768 && window.screen.width >= 576) {
+            if (window.pageYOffset >= 2400 && window.pageYOffset < 2800) {
+                activateProjImg1();
+            } else if (window.pageYOffset > 2800 && window.pageYOffset < 3200) {
+                activateProjImg2();
+            } else if (window.pageYOffset > 3200 && window.pageYOffset < 3600) {
+                activateProjImg3();
+            } else if (window.pageYOffset > 3600 && window.pageYOffset < 4200) {
+                activateProjImg4and5();
+            } else {
+                deactivateAllProjImgs();
             }
-            document.querySelector('#projGallerySec1').classList.remove('active');
-            document.querySelector('#projGallerySec2').classList.remove('active');
-            document.querySelector('#projGallerySec3').classList.remove('active');
-            document.querySelector('#projGallerySec4').classList.add('active');
-            document.querySelector('#projGallerySec5').classList.add('active');
-        } else {
-            for (var i = 0; i < navLinksColor.length; i++) {
-                navLinksColor[i].classList.remove('changeBg');
+        } else if (window.screen.width < 576) {
+            if (window.pageYOffset >= 2600 && window.pageYOffset < 3000) {
+                activateProjImg1();
+            } else if (window.pageYOffset > 3000 && window.pageYOffset < 3400) {
+                activateProjImg2();
+            } else if (window.pageYOffset > 3400 && window.pageYOffset < 3800) {
+                activateProjImg3();
+            } else if (window.pageYOffset > 3800 && window.pageYOffset < 4200) {
+                activateProjImg4();
+            } else if (window.pageYOffset > 4200 && window.pageYOffset < 4600) {
+                activateProjImg5();
+            } else {
+                deactivateAllProjImgs();
             }
-            document.querySelector('#projGallerySec1').classList.remove('active');
-            document.querySelector('#projGallerySec2').classList.remove('active');
-            document.querySelector('#projGallerySec3').classList.remove('active');
-            document.querySelector('#projGallerySec4').classList.remove('active');
-            document.querySelector('#projGallerySec5').classList.remove('active');
-        }
+        } 
     }
 
     // Project Slider Controls
@@ -150,4 +175,100 @@ function setProjSliderImg() {
         document.getElementById('projSliderTitle').innerHTML = 'Tudor Consulting';
         document.getElementById('projSliderLink').setAttribute('href', '/tudorconsulting/');
     }
+}
+
+// Set active state for project 1 image section
+function activateProjImg1() {
+    for (var i = 0; i < navLinksColor.length; i++) {
+        navLinksColor[i].classList.remove('changeBg');
+    }
+    document.querySelector('#projGallerySec1').classList.add('active');
+    document.querySelector('#projGallerySec2').classList.remove('active');
+    document.querySelector('#projGallerySec3').classList.remove('active');
+    document.querySelector('#projGallerySec4').classList.remove('active');
+    document.querySelector('#projGallerySec5').classList.remove('active');
+}
+
+// Set active state for project 2 image section
+function activateProjImg2() {
+    for (var i = 0; i < navLinksColor.length; i++) {
+        navLinksColor[i].classList.remove('changeBg');
+    }
+    document.querySelector('#projGallerySec1').classList.remove('active');
+    document.querySelector('#projGallerySec2').classList.add('active');
+    document.querySelector('#projGallerySec3').classList.remove('active');
+    document.querySelector('#projGallerySec4').classList.remove('active');
+    document.querySelector('#projGallerySec5').classList.remove('active');
+}
+
+// Set active state for project 3 image section
+function activateProjImg3() {
+    for (var i = 0; i < navLinksColor.length; i++) {
+        navLinksColor[i].classList.remove('changeBg');
+    }
+    document.querySelector('#projGallerySec1').classList.remove('active');
+    document.querySelector('#projGallerySec2').classList.remove('active');
+    document.querySelector('#projGallerySec3').classList.add('active');
+    document.querySelector('#projGallerySec4').classList.remove('active');
+    document.querySelector('#projGallerySec5').classList.remove('active');
+}
+
+// Set active state for project 4 image section
+function activateProjImg4() {
+    for (var i = 0; i < navLinksColor.length; i++) {
+        navLinksColor[i].classList.remove('changeBg');
+    }
+    document.querySelector('#projGallerySec1').classList.remove('active');
+    document.querySelector('#projGallerySec2').classList.remove('active');
+    document.querySelector('#projGallerySec3').classList.remove('active');
+    document.querySelector('#projGallerySec4').classList.add('active');
+    document.querySelector('#projGallerySec5').classList.remove('active');
+}
+
+// Set active state for project 5 image section
+function activateProjImg5() {
+    for (var i = 0; i < navLinksColor.length; i++) {
+        navLinksColor[i].classList.remove('changeBg');
+    }
+    document.querySelector('#projGallerySec1').classList.remove('active');
+    document.querySelector('#projGallerySec2').classList.remove('active');
+    document.querySelector('#projGallerySec3').classList.remove('active');
+    document.querySelector('#projGallerySec4').classList.remove('active');
+    document.querySelector('#projGallerySec5').classList.add('active');
+}
+
+// Set active state for project 4 and 5 image section
+function activateProjImg4and5() {
+    for (var i = 0; i < navLinksColor.length; i++) {
+        navLinksColor[i].classList.remove('changeBg');
+    }
+    document.querySelector('#projGallerySec1').classList.remove('active');
+    document.querySelector('#projGallerySec2').classList.remove('active');
+    document.querySelector('#projGallerySec3').classList.remove('active');
+    document.querySelector('#projGallerySec4').classList.add('active');
+    document.querySelector('#projGallerySec5').classList.add('active');
+}
+
+// Set active state for project 5 image section
+function deactivateAllProjImgs() {
+    for (var i = 0; i < navLinksColor.length; i++) {
+        navLinksColor[i].classList.remove('changeBg');
+    }
+    document.querySelector('#projGallerySec1').classList.remove('active');
+    document.querySelector('#projGallerySec2').classList.remove('active');
+    document.querySelector('#projGallerySec3').classList.remove('active');
+    document.querySelector('#projGallerySec4').classList.remove('active');
+    document.querySelector('#projGallerySec5').classList.remove('active');
+}
+
+// Deactivate all project image sections and set nav link bg color active
+function changeNavColor() {
+    for (var i = 0; i < navLinksColor.length; i++) {
+        navLinksColor[i].classList.add('changeBg');
+    }
+    document.querySelector('#projGallerySec1').classList.remove('active');
+    document.querySelector('#projGallerySec2').classList.remove('active');
+    document.querySelector('#projGallerySec3').classList.remove('active');
+    document.querySelector('#projGallerySec4').classList.remove('active');
+    document.querySelector('#projGallerySec5').classList.remove('active');
 }
